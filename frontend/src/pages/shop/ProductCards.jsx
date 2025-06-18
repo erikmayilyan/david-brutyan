dc8b65b85e4e7a76004d287fc8b86c30cef78fd6
Erik <mayilyane9@gmail.com>
2025-06-18 15:29:18 +0200

    Add inline styles as fallback for flexbox layout

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,9 +1,9 @@
+import './ProductCards.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '../../LanguageContext';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../redux/features/cart/cartSlice';
-import './ProductCards.css';
 
 const ProductCards = ({ products }) => {
   const { language } = useLanguage();
@@ -19,10 +19,20 @@ const ProductCards = ({ products }) => {
   };
 
   return (
-    <div className='product-cards'>
+    <div className='product-cards' style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '3rem',
+      marginTop: '70px',
+      width: '100%'
+    }}>
       {
         products.map((product, index) => (
-          <div key={index} className='product-card'>
+          <div key={index} className='product-card' style={{
+            flex: '1 1 100%',
+            maxWidth: '100%',
+            marginBottom: '0'
+          }}>
             <div className='relative'>
               <Link to={`${getLanguagePrefix()}/shop/${product._id}`}>
                 <img src={product.image} alt="product image" className='product-image' />
